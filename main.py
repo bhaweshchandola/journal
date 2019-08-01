36c8a5e995075c27cd6072ad0a7011ac232294a8
bhaweshchandola <bhawesh.chandola@gmail.com>
2019-08-01 23:07:25 +0530

    login added

--- main.py
+++ main.py
@@ -14,8 +14,23 @@ def signup():
         os.makedirs(directory)
         print("directory made")
     f = open(directory+"/users.txt", "a+")
-    user_details = {"username":user_name, "password":password}
-    f.write(json.dumps(user_details))
+    user_details = user_name+" "+password + '\n'
+    f.write(user_details)
     f.close()
 
-signup()
+
+def login():
+    user_name = input("Plase Enter your Username")
+    password = input("Please Enter your Password")
+
+    for line in open("users/users.txt", 'r').readlines():
+        temp_info = line.split()
+        if user_name == temp_info[0] and password == temp_info[1]:
+            print("user authenticated")
+            return True
+
+    print("Authentication Failed")
+    return False
+
+
+login()
