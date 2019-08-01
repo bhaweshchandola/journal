fe74d0ebe4f42c15c2388512cb9a3f48dfb155ad
bhaweshchandola <bhawesh.chandola@gmail.com>
2019-08-01 22:40:20 +0530

    user signup

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import numpy as np
+import os
+import json
 
 print("hello")
+
+
+def signup():
+    user_name = input("Plase Enter your Username")
+    password = input("Please Enter your Password")
+
+    directory = "users" 
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        print("directory made")
+    f = open(directory+"/users.txt", "a+")
+    user_details = {"username":user_name, "password":password}
+    f.write(json.dumps(user_details))
+    f.close()
+
+signup()
