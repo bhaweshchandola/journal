7f5227186c2f153dc1ff67bc15e683122dbecfa2
bhaweshchandola <bhawesh.chandola@gmail.com>
2019-08-03 01:40:01 +0530

    encrypt decrypt

--- main.py
+++ main.py
@@ -2,11 +2,41 @@ import numpy as np
 import os
 import json
 import datetime
+from cryptography.fernet import Fernet
+
 
 print("hello")
 
 journal_direc = "journals"
 users_direc = "users"
+key_dir = "key"
+
+
+def gen_key():
+    if not os.path.exists(key_dir):
+        key = Fernet.generate_key()
+        os.makedirs(key_dir)
+        print("directory made")
+        f = open(key_dir+"/key.txt", "a+")
+        f.write(key.decode())
+    return True
+
+
+
+def encrypt(inp_str):
+    key = open(key_dir+"/key.txt", 'r').readline()
+    cipher_suite = Fernet(key)
+    encoded_text = cipher_suite.encrypt(inp_str.encode())
+    
+    return encoded_text.decode()
+
+
+def decrypt(enc_str):
+    key = open(key_dir+"/key.txt", 'r').readline()
+    cipher_suite = Fernet(key)
+    decoded_text = cipher_suite.decrypt(enc_str.encode())
+    return decoded_text.decode()
+
 
 def signup():
     user_name = input("Plase Enter your Username")
@@ -55,10 +85,14 @@ def journal_entry(username):
         print("directory made")
     f = open(journal_direc+"/"+username+".txt", 'a+')
     temp_entry = input("Type your Journal Entry")
+    check_entry(username)
     entry = datetime.datetime.now().strftime("%d %b %Y %I.%M%p").lower() + " - " + temp_entry
     f.write(entry)
     f.write('\n')
     f.close()
 
 # journal_entry(login())
-check_entry("bh")
+# check_entry("bh")
+# gen_key()
+# print(encrypt("helllooooo"))
+print(decrypt('gAAAAABdRJgHyL-e2a4fls57915932x4zCb01Mt-_OozscBp6cACuhnv432qO759slrjDwwQR7BQaCdmQt_ycR4RnRzr1YfYzA=='))
