b9d9eb1067ac384456ae8a09072645112444134c
bhaweshchandola <bhawesh.chandola@gmail.com>
2019-08-03 12:51:59 +0530

    final main

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from cryptography.fernet import Fernet
 
 print("hello")
 
-journal_direc = "journals"
-users_direc = "users"
-key_dir = "key"
+journal_direc = "journals" # journals directory
+users_direc = "users" $users directory
+key_dir = "key" #encryption key directory
 
 
+# function to generate key for encryption
 def gen_key():
     if not os.path.exists(key_dir):
         key = Fernet.generate_key()
@@ -21,7 +22,11 @@ def gen_key():
         f.write(key.decode())
     return True
 
-
+''' 
+function to encrypt a string
+param = input string
+returns encrypted string
+'''
 def encrypt(inp_str):
     key = open(key_dir+"/key.txt", 'r').readline()
     cipher_suite = Fernet(key)
@@ -30,6 +35,11 @@ def encrypt(inp_str):
     return encoded_text.decode()
 
 
+''' 
+function to decrypt a string
+param = encoded string
+returns orignal string
+'''
 def decrypt(enc_str):
     key = open(key_dir+"/key.txt", 'r').readline()
     cipher_suite = Fernet(key)
@@ -38,6 +48,12 @@ def decrypt(enc_str):
     return decoded_text.decode()
 
 
+'''
+function user signup
+takes user input username and password
+checks number of users whih already exists, limit of 10
+create a user by storing username and password in the users.txt file
+'''
 def signup():
     user_name = input("Plase Enter your Username: ")
     password = input("Please Enter your Password: ")
@@ -48,21 +64,27 @@ def signup():
     filename = users_direc + "/users.txt"
     num_lines = sum(1 for line in open(filename))
     
-    if num_lines > 15:
-        print("Users limit reached")
+    if num_lines > 10:
+        print("*********Users limit reached***********")
     else:
         for line in open(filename, 'r').readlines():
             temp_info = line.split()
             if user_name == temp_info[0]:
-                print("user already exists")
-                return user_name
+                print("*******User Already Exists*********")
+                return False
 
         f = open(filename, "a+")
         user_details = user_name+" "+encrypt(password) + '\n'
         f.write(user_details)
         f.close()
+        return user_name
 
 
+'''
+function for user login
+takes user input username and password
+checks if the username and password matches or not
+'''
 def login():
     user_name = input("Plase Enter your Username: ")
     password = input("Please Enter your Password: ")
@@ -80,10 +102,15 @@ def login():
     return False
 
 
+'''
+function to check number of entries in the journal
+param = username
+if greater than 50 delete the first entry
+'''
 def check_entry(username):
     filename = journal_direc +"/"+username+".txt"
     num_lines = sum(1 for line in open(filename))
-    print(num_lines)
+    # print(num_lines)
     if num_lines > 50:
         with open(filename, 'r') as fin:
             data = fin.read().splitlines(True)
@@ -92,6 +119,13 @@ def check_entry(username):
     return num_lines
 
 
+
+'''
+function to create a journal entry
+param = username
+creates a file with the username
+takes user input for jounal entry
+'''
 def journal_entry(username):
     # directory_j = "journals"
     if not os.path.exists(journal_direc):
@@ -106,6 +140,11 @@ def journal_entry(username):
     f.close()
 
 
+'''
+function to show journal log
+param = username
+read the user journal and dispay them in order
+'''
 def journal_show(username):
     filename = journal_direc + "/" + username + ".txt"
     if os.path.exists(filename):
@@ -118,35 +157,39 @@ def journal_show(username):
     else:
         print("No Entries Found")
 
-# journal_entry(login())
-# check_entry("bh")
-# gen_key()
-# print(encrypt("helllooooo"))
-# print(decrypt(encrypt("helllooooo")))
-# journal_show("bh")
-# signup()
-# login()
 
 
+'''
+main function
+prompts user with option to login, signup or exit
+then prompts user with option to view or create a new journal entry
+'''
 def main():
     main_call = True
+    user = False
     while main_call:
-        inp = int(input("Press 1 to Login \nPress 2 to Signup \nPress 3 to Exit\n"))
-        
-        if inp == 1:
-            user = login()
+    
+        if not user:
+            inp = int(input("Press 1 to Login \nPress 2 to Signup \nPress 3 to Exit\n"))
+    
+        if inp == 1 or user:
+            if not user:
+                user = login()
             if user:
                 in_call = True
                 while in_call:
+                    print()
                     inp1 = int(input("Press 1 to make a journal Entry\nPress 2 to view previous entries\nPress 3 to login as a new user\n"))
                     if inp1 == 1:
                         journal_entry(user)
                     elif inp1 == 2:
                         journal_show(user)
                     elif inp1 == 3:
+                        user = False
                         in_call = False
         elif inp == 2:
-            signup()
+            user = signup()
+            
         elif inp == 3:
             main_call = False
         else:
